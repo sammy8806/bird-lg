6694207ad81137a5feddd3ac201e5e92ca01fcb1
Baptiste Jonglez <baptiste--git@jonglez.org>
2015-02-11 23:41:15 +0100

    Allow to configure the location of Bird sockets

--- lgproxy.py
+++ lgproxy.py
@@ -96,9 +96,12 @@ def traceroute():
 def bird():
     check_accesslist()
 
-    if request.path == "/bird": b = BirdSocket(file="/var/run/bird.ctl")
-    elif request.path == "/bird6": b = BirdSocket(file="/var/run/bird6.ctl")
-    else: return "No bird socket selected"
+    if request.path == "/bird":
+        b = BirdSocket(file=app.config.get("BIRD_SOCKET", "/var/run/bird.ctl"))
+    elif request.path == "/bird6":
+        b = BirdSocket(file=app.config.get("BIRD6_SOCKET", "/var/run/bird6.ctl"))
+    else:
+        return "No bird socket selected"
 
     query = request.args.get("q","")
     query = unquote(query)
